4eab6a37059f0a9880fd76f2b3dac3c96632b7bd
filiberto <ares@unizar.es>
2018-09-02 19:18:54 +0200

    Files added

---
+++ moebius/moebius.sh
@@ -0,0 +1,41 @@
+#Computes the entanglement entropy after a Lorentz transformation of parameter zeta
+
+#!/bin/bash
+
+l=$1 #Length of the interval
+delta_l=$2
+l_max=$3
+
+zeta=$4
+delta_zeta=$5
+zeta_max=$6
+
+alpha=$7 #Parameter of the Rényi entropy
+
+while (( $(echo "$zeta <= $zeta_max" | bc)==1)) ; do
+
+   old=0
+   
+   echo zeta=$zeta
+   
+   rm moebius_correl.dat
+   touch moebius_correl.dat
+
+   while (( $(echo "$l <= $l_max" | bc)==1)) ; do
+     
+     echo $l
+   
+     cat math_moebius | sed "s%myzeta%$zeta%g" | sed "s%myl%$l%g" | sed "s%old_l%$old%g" | math
+
+     ./moebius $l $zeta $alpha
+
+     old=$l;
+  
+     l=$(echo "$l + $delta_l" | bc)
+   
+   done 
+   
+   l=$1
+   zeta=$(echo "$zeta+$delta_zeta" | bc)
+   
+done
---
+++ sublog/matrix_g.sh
@@ -0,0 +1,24 @@
+#!/bin/bash                                                                                                                                                 
+
+: 'This script computes a row of the Toeplitz matrix with symbol g_\nu'
+
+l_max=$1 #Dimension of the matrix
+nu=$2    #Exponent of the symbol
+
+: 'Each step of the following loop calls the Mathematica notebook
+   math_matrix_g that computes in parallel the entries of 
+   the row between old and l'
+
+l=$3
+delta_l=$4
+old=$5
+
+while (( $(echo "$l <= $l_max" | bc)==1)) ; do
+    echo $l
+
+    cat math_matrix_g |  sed "s%mil%$l%g" | sed "s%miold%$old%g" | sed "s%minu%$nu%g" | math
+
+    old=$l+1
+
+    l=$(echo "$l + $delta_l" | bc)
+done

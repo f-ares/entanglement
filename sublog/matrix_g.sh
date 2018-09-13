8126244d67b5f929cb4a4cc2e3605bffcc1d185e
filiberto <ares@unizar.es>
2018-09-14 01:38:00 +0200

    Files added

--- sublog/matrix_g.sh
+++ sublog/matrix_g.sh
@@ -1,13 +1,13 @@
 #!/bin/bash                                                                                                                                                 
 
-: 'This script computes a row of the Toeplitz matrix with symbol g_\nu'
+#This script computes a row of the Toeplitz matrix with symbol g_\nu
 
 l_max=$1 #Dimension of the matrix
 nu=$2    #Exponent of the symbol
 
-: 'Each step of the following loop calls the Mathematica notebook
-   math_matrix_g that computes in parallel the entries of 
-   the row between old and l'
+#Each step of the following loop calls the Mathematica notebook
+#math_matrix_g that computes in parallel the entries of 
+#the row between old and l
 
 l=$3
 delta_l=$4
@@ -16,7 +16,7 @@ old=$5
 while (( $(echo "$l <= $l_max" | bc)==1)) ; do
     echo $l
 
-    cat math_matrix_g |  sed "s%mil%$l%g" | sed "s%miold%$old%g" | sed "s%minu%$nu%g" | math
+    cat math_matrix_g |  sed "s%myl%$l%g" | sed "s%myold%$old%g" | sed "s%minu%$nu%g" | math
 
     old=$l+1
 

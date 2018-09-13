8a160fea520b6a2be54b28978f7b6c7267c5404c
filiberto <ares@unizar.es>
2018-09-14 01:40:25 +0200

    Files added

--- sublog/matrix_g.sh
+++ sublog/matrix_g.sh
@@ -16,7 +16,7 @@ old=$5
 while (( $(echo "$l <= $l_max" | bc)==1)) ; do
     echo $l
 
-    cat math_matrix_g |  sed "s%myl%$l%g" | sed "s%myold%$old%g" | sed "s%minu%$nu%g" | math
+    cat math_matrix_g |  sed "s%myl%$l%g" | sed "s%myold%$old%g" | sed "s%mynu%$nu%g" | math
 
     old=$l+1
 
